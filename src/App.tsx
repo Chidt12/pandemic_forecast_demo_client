4fe4af5319acb2c40a0941f7c74871ac006b76b6
chizitga <vipmath171@gmail.com>
2021-12-12 21:13:33 +0700

    sf

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import { defaults } from 'react-chartjs-2';
 defaults.animation = false;
 import { Line } from 'react-chartjs-2';
 import SelectSearch from 'components/SelectSearch';
+import { CgSpinnerTwoAlt } from 'react-icons/cg'; import 'react-icons/cg';
+import * as uuid from 'uuid';
 
 const options = {
   responsive: true,
@@ -27,11 +29,22 @@ const App = () => {
 
   const [selected_province, setSelectedProvince] = useState("0");
   const [next_days, setNextDays] = useState(10);
+  const [update_data, setUpdateData] = useState<string[]>([]);
+
+  const [on_loading, setOnLoading] = useState(false);
+
+  const [reload, setReload] = useState("");
 
   const provinces = useAsync(async () => {
     const res = await Fetch.post<{ label: string, value: number }[]>("/get-province", {})
     return res.data;
   }, [])
+
+  const state_2 = useAsync(async () => {
+    const res = await Fetch.post<string[]>("/get-dates", {})
+
+    return res.data
+  }, [reload])
 
   const state = useAsync(async () => {
     const res = await Fetch.post<{
@@ -80,11 +93,11 @@ const App = () => {
 
       for (let index = 0; index < labels.length; index++) {
         var element = labels[index];
-        if(element == "Null"){
-         var new_date = new Date((new Date()).setDate(start_date.getDate() + index))
+        if (element == "Null") {
+          var new_date = new Date((new Date()).setDate(start_date.getDate() + index))
           element = `${new_date.getFullYear()}-${new_date.getMonth() < 12 ? new_date.getMonth() + 1 : 1}-${new_date.getDate()}`;
         }
-        
+
         new_labels.push(element)
       }
 
@@ -97,31 +110,21 @@ const App = () => {
   }, [state.value])
 
   const onClick = async () => {
-    var res = await Fetch.post("/fetch-data", {
+    setOnLoading(true);
+    var res = await Fetch.post("/click-fetch", {
 
     });
+    setOnLoading(false);
+    setReload(uuid.v4())
 
-    console.log(res.data);
-  }
-
-  const onClickIncrease = async () => {
-    var res = await Fetch.post("/increase-test", {
-
-    })
-  }
-
-  const onClickTest = async () => {
-    var res = await Fetch.post("/realtime-data", {
-
-    })
   }
 
   return (
     <div className="App mt-10">
-      <div className="flex items-center">
+      <div className="flex items-start">
         <div className="lg:w-1/2 w-1/3 px-10">
           <div className="">
-            
+
             <div className="w-full">
               {provinces.value && <div>
                 <SelectSearch
@@ -133,7 +136,27 @@ const App = () => {
               </div>}
             </div>
             <div className="w-full mt-5">
-                <input type="number" placeholder="" value={next_days} name="" onChange={(e) => setNextDays(parseInt(e.target.value))} className="w-full outline-none focus:outline-none border border-black border-opacity-20 focus:border-opacity-90 px-4 py-2 rounded text-base" id=""  step={1} min={1}/>
+              <input type="number" placeholder="" value={next_days} name="" onChange={(e) => setNextDays(parseInt(e.target.value))} className="w-full outline-none focus:outline-none border border-black border-opacity-20 focus:border-opacity-90 px-4 py-2 rounded text-base" id="" step={1} min={1} />
+            </div>
+
+            <div className='mt-3'>
+              <button onClick={onClick} className='flex justify-center items-center px-3 py-1 rounded bg-primary font-medium text-white w-40'>
+                <span>Fetch Data</span>
+                {on_loading && <span className=' animate-spin'>
+                  <CgSpinnerTwoAlt />
+                </span>}
+              </button>
+            </div>
+
+            <div className='mt-3'>
+              <h3>Crawled Dates</h3>
+              <div className='h-52 overflow-y-auto'>
+                {state_2.value && state_2.value.map((date, index) => (
+                  <div className='mt-1.5'>
+                    {date}
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
@@ -144,6 +167,7 @@ const App = () => {
               <Line data={data} options={options} />
             )
           }
+
         </div>
       </div>
     </div>
